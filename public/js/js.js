38e3221d097a5d15a956ee1864700c69b540023b
Harsh sodha <harshsodha90@gmail.com>
2019-12-20 19:07:01 +0530

    Init commit

---
+++ public/js/js.js
@@ -0,0 +1,40 @@
+//console.log('i am fool')
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+response.json().then((data)=>{
+    console.log(data)
+})
+})
+    
+const form = document.querySelector('form')
+const search=document.querySelector('input')
+const mssgOne=document.querySelector('#mssg-1')
+const mssgTwo=document.querySelector('#mssg-2')
+//const mssgThree=document.querySelector('#mssg-3')
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const location=search.value
+
+    mssgOne.textContent='loading'
+    mssgTwo.textContent=''
+
+    fetch('http://localhost:3000/weather?address='+ location).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                //console.log(data.error)
+                mssgOne.textContent= data.error
+               // mssgTwo.textContent= data.error
+            }
+            else{
+             //   console.log(data.forecast)
+            //console.log(data.location)
+            mssgOne.textContent= data.forecast
+            mssgTwo.textContent=data.location
+            }
+        })
+    })
+console.log(location)
+
+})
---
+++ src/app2.js
@@ -0,0 +1,95 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+const request= require('request')
+
+const geocode = require('./geocode1')
+const forecast = require('./forecast1')
+const address= process.argv[2]
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPaths=path.join (__dirname,'../templates/partial')
+
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPaths)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'WELCOME PAGE!',
+        name: 'SODHA HARSH'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'HARSH SODHA'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+       name: 'HARSH SODHA.',
+       helpText: 'this is some helpful text'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address)
+    {
+      return  res.send({
+            error: 'please provide an address'
+      })
+    }
+    geocode(req.query.address,(error,{longitude,latitude,location}={}) =>{
+        if(error){
+         return res.send({
+              error
+         })
+         // console.log('data',data)
+        }
+        
+          forecast(latitude,longitude, (error ,forecastdata) =>
+        {
+          if(error){
+            res.send({
+              error
+            })
+          }
+             res.send({
+            forecast: forecastdata,location,
+            address: req.query.address
+          })
+          //console.log(location)
+         // console.log(forecastdata)
+            
+        })
+        })
+})
+app.get('/help/*', (req, res) => {
+    res.render('404',{
+        title: '404',
+        name:'HARSH SODHA',
+        errorMssg: 'could not find the r8 content in help page'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404',{
+        errorMssg: 'could not find the page'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
